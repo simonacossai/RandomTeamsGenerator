f12d276050be82d5b13dd5fc51bb75e91683d64a
simonacossai <cossaisimona@gmail.com>
2020-10-15 16:22:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let teams= document.querySelector("select");
+    let names= document.querySelector("textarea");
+    let button = document.querySelector(".btn");
+    let btn = document.querySelector(".toggle-person");
+    let availableDiv = document.querySelector(".available");
+    const printValue=()=>{
+    let namesArray = names.value.split(" ");
+    availableDiv.innerHTML=namesArray;
+    let row= document.querySelector(".teams");
+    let divs=[];
+    let numberOfDivs= teams.value;
+    for(let i=0; i<numberOfDivs; i++){
+        let randomDiv = document.createElement("div");
+        randomDiv.classList.add("col");
+        divs.push(randomDiv);     
+    }
+
+    for(let i=0; i<divs.length; i++){
+        row.appendChild(divs[i]);
+    }
+    teams.value=""
+}
+
+
+const takeRandomPerson=()=>{
+    let arrayOfPerson = availableDiv.innerHTML.split(",")
+    let teamDivs = document.querySelectorAll(".col");
+    let randomIndex = Math.floor(Math.random() * arrayOfPerson.length);
+    let randomItem = arrayOfPerson.splice(randomIndex, 1)[0];
+    availableDiv.innerHTML=arrayOfPerson;
+
+    let randomDiv = Math.floor(Math.random() * teamDivs.length);
+    teamDivs[randomDiv].innerHTML+=randomItem+ ", ";
+}
+
